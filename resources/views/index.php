ec8ad6de3d1b4d613e97b02f3fa8e95aa74e3df8
Oleg Zolotarev <zolotarev.om@gmail.com>
2015-09-22 15:22:26 +0300

    add & update views

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,12 +1,37 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="ru">
 <head>
-    <meta charset="UTF-8">
     <title>Main page</title>
+    <meta charset="UTF-8">
+    <meta name=description content="Burn After Reading">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" media="screen">
 </head>
 <body>
-
-<?php echo $test ?>
-
+<div class="container">
+    <div class="col-md-8">
+        <div class="row center-block">
+            <?php
+            if (!empty($text)) {
+                echo $text;
+            } elseif (!empty($url)) {
+                include 'inc.new.php';
+            } else {
+                include 'inc.form.php';
+                if (isset($errors) && count($errors) > 0) {
+                    foreach ($errors as $error) {
+                        include 'inc.error.php';
+                    }
+                }
+            }
+            ?>
+        </div>
+    </div>
+</div>
+<!-- jQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script>
+<!-- Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </body>
 </html>
